9976d49a3eab4cb8da6a02232851493585ad3023
Lala <lalaaagh@code.edu.az>
2024-03-11 22:51:23 +0400

    Initial Commit

---
+++ AzObservation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AzObservation
+//
+//  Created by Lala on 11.03.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
